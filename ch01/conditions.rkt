023d7577e7fac77b83ed0bca7cfaa4e5a6b7ba16
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-11-18 08:44:30 -0500

    feat: more conditions

--- ch01/conditions.rkt
+++ ch01/conditions.rkt
@@ -4,3 +4,20 @@
           ((< x 0) (- x))))
 
 (display(abs (- 5)))
+
+(newline)
+
+(define (abs x)
+    (cond ((< x 0) (- x))
+          (else x)))
+
+(display(abs (- 5)))
+
+(newline)
+
+(define (abs x)
+    (if (< x 0)
+        (- x)
+        x))
+
+(display(abs (- 5)))
