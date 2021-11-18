92e0ebce990357adf0d084552b86463aa37beaef
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-11-18 08:06:49 -0500

    feat: circumference and sum of squares

---
+++ ch01/circumference.rkt
@@ -0,0 +1,5 @@
+(define pi 3.14159)
+(define radius 10)
+(* pi (* radius radius))
+(define circumference (* 2 pi radius))
+(display circumference)
---
+++ ch01/sumofsquares.rkt
@@ -0,0 +1,9 @@
+(define (square x) (* x x))
+(display(square 21))
+
+(newline)
+
+(define (sum-of-squares x y)
+    (+ (square x) (square y)))
+
+(display(sum-of-squares 3 4))
