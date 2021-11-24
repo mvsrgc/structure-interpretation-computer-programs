40040bc10a114697ba4dbc7cd342d5d5b9b3f70f
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-11-24 14:51:01 -0500

    feat: exponential log time

--- 1.2/exponential.rkt
+++ 1.2/exponential.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (define (expt b n)
-    (expt-iter b n 1))
+    ;(expt-iter b n 1))
+    (fast-expt b n))
 
 (define (expt-iter b counter product)
     (if (= counter 0)
@@ -10,3 +11,17 @@
             b
             (- counter 1)
             (* product b))))
+
+(define (even? n) 
+    (= (remainder n 2) 0))
+
+(define (square x)
+    (* x x))
+
+(define (fast-expt b n)
+    (cond 
+        ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(expt 2 2)
