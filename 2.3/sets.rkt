e531e747ffe3158cb48eded1dabd3f1ad878514d
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-12-14 17:29:21 -0500

    feat: intersection set

--- 2.3/sets.rkt
+++ 2.3/sets.rkt
@@ -17,3 +17,12 @@
     (if (element-of-set? x set) 
         set 
         (cons x set)))
+
+(define (intersection-set set1 set2)
+    (cond
+        [(or (null? set1) (null? set2)) '()]
+        [(element-of-set? (car set1) set2)
+            (cons (car set1) (intersection-set (cdr set1) set2))]
+        [else (intersection-set (cdr set1) set2)]))
+
+(intersection-set (list 1 2 3) (list 1 2 3 4))
